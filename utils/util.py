3ef8c96114f5bda7f384a7f146add7026cb47d03
Namba <s1511303@s.tsukuba.ac.jp>
2019-06-09 22:05:56 +0900

    不要ボタンを削除

--- utils/util.py
+++ utils/util.py
@@ -5,9 +5,11 @@ class Utils():
         errMsg = []
         noInput = 'が未入力です。'
         if not data['name']:
-            errMsg.append('書名' + noInput)
+            errMsg.append('甘さ' + noInput)
+        if not data['volume']:
+            errMsg.append('辛さ' + noInput)
         if not data['author']:
-            errMsg.append('著者' + noInput)
+            errMsg.append('酸っぱさ' + noInput)
         if not data['publisher']:
-            errMsg.append('出版社' + noInput)
+            errMsg.append('酸っぱさ' + noInput)
         return errMsg
